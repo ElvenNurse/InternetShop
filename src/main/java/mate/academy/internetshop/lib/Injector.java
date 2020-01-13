b8eb082409341cebff9fd5bea2edc21399091103
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2020-01-13 15:15:24 +0200

    Added log4j2 support, replaced System.Out and e.PrintStackTrace() to logger

--- src/main/java/mate/academy/internetshop/lib/Injector.java
+++ src/main/java/mate/academy/internetshop/lib/Injector.java
@@ -8,7 +8,11 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Injector {
+    private static final Logger logger = LogManager.getLogger(Injector.class);
     private static final String PROJECT_MAIN_PACKAGE = "mate.academy.internetshop";
     private static List<Class> classes = new ArrayList<>();
 
@@ -16,7 +20,7 @@ public class Injector {
         try {
             classes.addAll(getClasses(PROJECT_MAIN_PACKAGE));
         } catch (ClassNotFoundException | IOException e) {
-            e.printStackTrace();
+            logger.error(e);
         }
     }
 
